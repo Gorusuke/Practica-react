c4bb586915b996becfaec4fd77c4af5cfd848f49
Jose Andres <Gorusuke123@gmail.com>
2020-12-22 01:35:12 -0500

    Reestructurando el codigo para una busqueda mejor de las herramientas implementadas

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
-import PaginaCursos from './PaginaCursos.jsx';
-import Formulario from './Formulario.jsx';
-import Error404 from './Error404.jsx';
-import Curso from './Curso.jsx';
+import Formulario from './Components/Pages/Formulario.jsx';
+import Curso from './Components/Pages/Curso.jsx';
+import CourseGrid from './Components/Pages/CourseGrid.jsx';
+import Error404 from './Components/Pages/Error404.jsx';
 // import './styles/styles.scss';
 // import './app.css';
 
@@ -16,7 +16,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={Formulario}/>
         <Route exact path="/pagina-cursos/:id" component={Curso}/>
-        <Route exact path="/pagina-cursos" component={PaginaCursos}/>
+        <Route exact path="/pagina-cursos" component={CourseGrid}/>
         <Route component={Error404}/>
       </Switch>
     </Router>
