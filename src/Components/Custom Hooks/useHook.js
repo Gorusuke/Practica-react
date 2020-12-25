448d16e7440ca9a611fadc3ebf9ed923d2ae3ed9
Jose Andres <Gorusuke123@gmail.com>
2020-12-24 22:23:44 -0500

    agregando la carpeta de custom hook

---
+++ src/Components/Custom Hooks/useHook.js
@@ -0,0 +1,24 @@
+import {useEffect, useState} from 'react';
+
+const useHook = (id) => {
+    const [cursos, setCursos] = useState([]);
+
+    const API = async (ident) => {
+        let informacion;
+        {ident
+            ? informacion = await fetch(`http://localhost:3000/cursos/${ident}`)
+            : informacion = await fetch(`http://localhost:3000/cursos`)
+        }
+        const resultado = await informacion.json();
+        setCursos(resultado);
+    }
+
+    useEffect(() => {
+        API(id)
+    }, []);
+
+    return cursos;
+
+}
+ 
+export default useHook;
