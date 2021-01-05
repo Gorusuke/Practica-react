c24044000a98dcf75100f5b27064588751e8b26d
Jose Andres <Gorusuke123@gmail.com>
2021-01-05 00:00:32 -0500

    Al parecer no se puede tener el manejo de la Api a nivel global, procediendo a dejar todo como estaba

--- src/Components/ContextAPI/Context/CourseReducer.js
+++ src/Components/ContextAPI/Context/CourseReducer.js
@@ -4,11 +4,10 @@ import {GET_COURSES} from '../Types/index'
 export default (state, action) => {
     switch (action.type) {
         case GET_COURSES:
-            console.info(action)
-            // return{
-            //     ...state,
-            //     courses: action.payload
-            // }
+            return{
+                ...state,
+                courses: action.payload
+            }
         default:
             return state;
     }
